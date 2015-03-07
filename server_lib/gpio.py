47d993432cf139c3bc65f528aaf426db57c53624
therealtylerdurden <therealtylerdurden@users.noreply.github.com>
2015-03-06 19:03:51 -0600

    organized

---
+++ server_lib/gpio.py
@@ -0,0 +1,22 @@
+import RPi.GPIO as GPIO
+
+class pin(object):
+	def __init__(self, num, iotype, state):
+		self.num = num
+		self.iotype = iotype
+		self.state = state
+
+def parse(data):
+	pnumber = int((data.split('=')[1]).split(',')[0])
+	ptype = data.split(',')[1]
+	pstate = int(data.split(',')[2])
+	p = pin(pnumber, ptype, pstate)
+	return p
+
+def toggle(p):
+	if p.iotype == "OUT":
+       		GPIO.setup(p.num, GPIO.OUT)
+        	GPIO.output(p.num, p.state)
+
+	elif p.iotype == "IN":
+		return GPIO.input(p.num)
---
+++ server_lib/server.py
@@ -0,0 +1,33 @@
+import gpio
+import socket
+
+TCP_IP = '0.0.0.0'
+TCP_PORT = 5432
+BUFFER_SIZE = 1024
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+s.bind((TCP_IP, TCP_PORT))
+s.listen(1)
+
+
+while 1:
+    conn, addr = s.accept()
+    print 'Connection address:', addr
+    
+    data = (conn.recv(BUFFER_SIZE)).strip()
+    print(data)
+    
+    if data:
+        
+        cmd = data.split('=')[0]
+       
+        if cmd == "PIN":
+            p = gpio.parse(data)
+            gpio.toggle(p)
+
+    else:
+        break
+    
+    conn.close()
