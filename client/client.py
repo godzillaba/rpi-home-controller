bea123439f83956d99a03f9ee9f1e370c2fc1bba
Henry Arneson <therealtylerdurden@users.noreply.github.com>
2015-03-10 13:05:44 -0500

    Update client.py

--- client/client.py
+++ client/client.py
@@ -1,24 +1,25 @@
 #!/usr/bin/env python
-
 import socket
 import sys
 
+def usage():
+	print "Usage: python client.py <hostname or ip> <port> <message>"
 
+try:
+	host = sys.argv[1]
+	TCP_IP = socket.gethostbyname(host)
+	TCP_PORT = int(sys.argv[2])
+	BUFFER_SIZE = 1024
+	MESSAGE = sys.argv[3]
 
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect((TCP_IP, TCP_PORT))
+	s.send(MESSAGE)
+	s.close()
 
-pin = 37
-hilo = 1
-
-
-host = 'pi'
-TCP_IP = socket.gethostbyname(host)
-TCP_PORT = 5432
-BUFFER_SIZE = 1024
-
-MESSAGE = "PIN=%s,OUT,%s" % (pin, hilo)
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((TCP_IP, TCP_PORT))
-s.send(MESSAGE)
-s.close()
-
+except NameError:
+	usage()
+except IndexError:
+	usage()
+except ValueError:
+	usage()
