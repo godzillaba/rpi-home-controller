22e546bfcbba55ba9d984c4b7325ff68df0e8654
therealtylerdurden <therealtylerdurden@users.noreply.github.com>
2015-03-29 12:18:49 -0500

    android client w/ busybox + nc

---
+++ client/android-client.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+host='pi'
+port='5432'
+
+sendcmd () {
+	echo "PIN=$1,OUT,$2" | nc $host $port
+}
+
+getstate () {
+	state=$(echo "PIN=$1,IN,0" | nc $host $port)
+	echo $state
+}
+
+if [ "$2" == '-' ]; then
+	pinstate=$(getstate $1)
+	if [ $pinstate == '0' ]; then
+		sendcmd $1 1
+	elif [ $pinstate == '1' ]; then
+		sendcmd $1 0
+	fi
+else
+	sendcmd $1 $2
+fi
