22f5ce517fd52fe833669fc6f83900427c026f40
therealtylerdurden <therealtylerdurden@users.noreply.github.com>
2015-04-09 00:08:53 +0000

    organized, WORKING

--- server.py
+++ server.py
@@ -4,9 +4,12 @@ import socket
 import logging
 import RPi.GPIO as GPIO
 
+# LOGGING / CONFIG VARIABLES
 f = "server_lib/config.conf"
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 
+
+# FUNCTIONS
 def toggle_all_relays(hilo):
 	relaypins = (conf.arg(f, "RELAYPINS")).split()
 	for pin in relaypins:
@@ -33,20 +36,24 @@ def serve_forever():
 	
 	conn.close()
 
-toggle_all_relays(1)
 
+### START ###
+
+# SET UP TCP SOCKET
 TCP_IP = conf.arg(f, "LISTENADDR")
 TCP_PORT = int(conf.arg(f, "PORT"))
 BUFFER_SIZE = int(conf.arg(f, "BUFFERSIZE"))
-
-
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind((TCP_IP, TCP_PORT))
 s.listen(1)
 logging.info('TCP server listening on %s:%s', TCP_IP, TCP_PORT)
 
+# SET UP RELAYS
+toggle_all_relays(0)
 
+
+# SERVE
 try:
     serve_forever()
         
