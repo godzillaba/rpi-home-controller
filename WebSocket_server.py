b6eabd00a1d908eb22f637666045daf35f2efb3c
therealtylerdurden <therealtylerdurden@users.noreply.github.com>
2015-04-27 01:02:59 +0000

    added settings page

--- WebSocket_server.py
+++ WebSocket_server.py
@@ -14,6 +14,7 @@ class MyServerProtocol(WebSocketServerProtocol):
         print("WebSocket connection open.")
 
     def onMessage(self, payload, isBinary):
+        global json_out
         print("Text message received: {0}".format(payload.decode('utf8')))
         cmd = payload.split('=')[0]
 
@@ -25,6 +26,15 @@ class MyServerProtocol(WebSocketServerProtocol):
                 self.sendMessage("PIN %s %s" % (p.num, toggle_output))
         elif cmd == "ALLRELAYS":
             gpio.toggle_all_relays(int(payload.split('=')[1]))
+        elif cmd == "GETJSON":
+                self.sendMessage("JSON----------" + json_out)
+        elif cmd == "SAVEJSON":
+                jsontext = payload.split('=')[1]
+                json_in = json.loads(jsontext)
+                json_out = json.dumps(json_in, indent=4)
+                with open('data.json', 'w') as data_file:
+                        data_file.truncate()
+                        data_file.write(json_out)
 
 
     def onClose(self, wasClean, code, reason):
@@ -32,6 +42,7 @@ class MyServerProtocol(WebSocketServerProtocol):
 
 with open('data.json') as data_file:
     data = json.load(data_file)
+    json_out = json.dumps(data, indent=4)
 
 port = int(data['WebSocket']['port'])
 address = "ws://localhost:%s" % port
---
+++ web/templates/settings/script.js
@@ -0,0 +1,55 @@
+var socket = null;
+var isopen = false;
+
+var get_json_data = function () {
+	socket.send("GETJSON")
+}
+
+var send_json_data = function () {
+	json.HTTP.port = document.getElementById("HTTP_Port").value
+	json.WebSocket.port = document.getElementById("WebSocket_Port").value
+	json.TCP.port = document.getElementById("TCP_Port").value
+	
+	json.Web.UI.NavColor = document.getElementById("NavColor").value
+	json.Web.UI.BodyColor = document.getElementById("BodyColor").value
+	
+	jsonstring = JSON.stringify(json)
+	console.log(jsonstring)
+	socket.send("SAVEJSON=" + jsonstring)
+}
+
+window.onload = function() {
+    hostname = window.location.hostname
+	var wsport = document.getElementById("wsport").content
+    
+	socket = new WebSocket("ws://" + hostname + ":" + wsport);
+    socket.binaryType = "arraybuffer";
+
+    socket.onopen = function() {
+	    console.log("Connected!");
+        isopen = true;
+		Materialize.toast("Connected to ws://" + hostname + ":" + wsport, 3000)
+		get_json_data()
+    }
+
+    socket.onmessage = function(e) {
+        if (typeof e.data == "string") {
+            console.log("Message received: " + e.data);
+            
+			if (e.data.split("----------")[0] === "JSON") {
+				data = e.data.split("----------")[1];
+				json = JSON.parse(data)
+				console.log(json)
+				Materialize.toast("Received JSON data", 3000)
+			}
+        }
+    }
+
+    socket.onclose = function(e) {
+        console.log("Connection closed.");
+		socket = null;
+        isopen = false;
+		Materialize.toast("Connection to ws://" + hostname + ":" + wsport + " closed.", 5000)
+    }
+};
+
