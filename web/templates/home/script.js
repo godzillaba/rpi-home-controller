0dad359e0ee098463a4121e8393ff2b32728c0b0
C0dePizza <C0dePizza@users.noreply.github.com>
2015-05-05 19:00:08 +0000

    organize js in prep for adding multiple ws connections

--- web/templates/home/script.js
+++ web/templates/home/script.js
@@ -1,51 +1,42 @@
-var socket = null;
-var isopen = false;
+function create (s) {
 
-window.onload = function() {
-    
-    // define and open main websocket connection
-    hostname = window.location.hostname
-	var wsport = document.getElementById("wsport").content
-    socket = new WebSocket("ws://" + hostname + ":" + wsport);
-    
-    function test (socket) {
-
-    socket.binaryType = "arraybuffer";
-
-    socket.onopen = function() {
-        console.log("Connected!");
-        isopen = true;
-		Materialize.toast("Connected to ws://" + hostname + ":" + wsport, 3000)
-        get_switch_stats()
-    }
-    
-    socket.onmessage = function(e) {
-            console.log("Message received: " + e.data);
-            
-            // parse received data
-            if (e.data.split(" ")[0] === "PIN") {
-                var pin_number = e.data.split(" ")[1]
-                var pin_status = e.data.split(" ")[2]
-                
-                // select checkbox pertaining to gpiopin and toggle
-                var pin_switch = (document.getElementsByName(pin_number)[0])
-                if (pin_status === "0") {
-                    pin_switch.checked = true
-                } else if (pin_status === "1") {
-                    pin_switch.checked = false
-                }
-            }
-        
-    }
-    
-    socket.onclose = function(e) {
-        console.log("Connection closed.");
+	s.binaryType = "arraybuffer";
+	
+	s.onopen = function() {
+	    console.log("Connected!");
+	    isopen = true;
+		Materialize.toast("Connected to " + s.url, 3000)
+	    get_switch_stats()
+		s.isopen = true
+	}
+	
+	s.onmessage = function(e) {
+	        console.log("Message received: " + e.data);
+	        
+	        // parse received data
+	        if (e.data.split(" ")[0] === "PIN") {
+	            var pin_number = e.data.split(" ")[1]
+	            var pin_status = e.data.split(" ")[2]
+	            
+	            // select checkbox pertaining to gpiopin and toggle
+	            var pin_switch = (document.getElementsByName(pin_number)[0])
+	            if (pin_status === "0") {
+	                pin_switch.checked = true
+	            } else if (pin_status === "1") {
+	                pin_switch.checked = false
+	            }
+	        }
+	    
+	}
+	
+	s.onclose = function(e) {
+	    console.log("Connection closed.");
 		socket = null;
-        isopen = false;
-		Materialize.toast("Connection to ws://" + hostname + ":" + wsport + " closed.", 5000)
-    }}
-    test(socket)
-};
+	    isopen = false;
+		Materialize.toast("Connection to " + s.url + " closed.", 5000)
+	}
+}
+
 
 
 // get status of each switch's gpiopin
@@ -65,7 +56,7 @@ window.setInterval(function () {
 
 // send command to ws server to toggle gpio pin when switch is clicked
 var switch_onclick = function(box) {
-    if (isopen === false) {
+    if (socket.isopen != true) {
         Materialize.toast("Not connected to WebSocket server", 5000)
     }
     if (box.checked === true) {
@@ -74,3 +65,19 @@ var switch_onclick = function(box) {
         socket.send('PIN=' + box.name + ',OUT,1')
     }
 }
+
+
+// start
+
+
+window.onload = function() {
+    
+    // define and open main websocket connection
+    hostname = window.location.hostname
+	var wsport = document.getElementById("wsport").content
+    socket = new WebSocket("ws://" + hostname + ":" + wsport);
+    create(socket)
+};
+
+
+
